b31d6c26c915352f7e276a35f63341543ef5f125
Tu Nombre <tu_correo@ejemplo.com>
2026-02-15 21:21:52 -0500

    utils listo

---
+++ src/utils.py
@@ -0,0 +1,29 @@
+import cv2
+
+def preprocess_image(image_path):
+    """
+    Preprocesamiento de imagen:
+    - Escala de grises
+    - Suavizado
+    - Umbralización adaptativa
+    """
+    image = cv2.imread(image_path)
+
+    if image is None:
+        raise FileNotFoundError("No se pudo cargar la imagen.")
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    thresh = cv2.adaptiveThreshold(
+        blurred,
+        255,
+        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        cv2.THRESH_BINARY,
+        11,
+        2
+    )
+
+    return thresh
+
